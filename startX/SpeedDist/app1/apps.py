ade243e6542c494e3d5509d4fb70c444e39d9455
evan <1442478313@qq.com>
2019-04-29 00:27:54 +0800

    功能优化

--- startX/SpeedDist/app1/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-from django.utils.module_loading import autodiscover_modules
-
-
-class App1Config(AppConfig):
-    name = 'app1'
-
-    def ready(self):
-        autodiscover_modules('startX')
