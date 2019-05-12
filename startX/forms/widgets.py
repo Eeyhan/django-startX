5663de5888a747d2bfc1830f770c6d433877ffac
evan <1442478313@qq.com>
2019-05-12 22:41:49 +0800

    添加kindeditor组件

--- startX/forms/widgets.py
+++ startX/forms/widgets.py
@@ -5,3 +5,7 @@ from django import forms
 
 class DateTimePickerInput(forms.TextInput):
     template_name = 'startX/forms/widgets/datetime_picker.html'
+
+class KindEditorInput(forms.Textarea):
+    template_name = 'startX/forms/widgets/kindeditor.html'
+
